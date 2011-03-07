67d5a124ff0d06810d7dc3288f1d75045b253f8c
José Fonseca <jose.r.fonseca@gmail.com>
2011-03-07 10:31:24 +0000

    Update build script to build x64 binaries too.

--- build.cmd
+++ build.cmd
@@ -1,2 +1,5 @@
-cmake -G "Visual Studio 9 2008" -H%CD% -B%CD%\build -DWINDBG_SDK_DIR=%CD%\sdk
-cmake --build build --config RelWithDebInfo --target stackdump
+cmake -G "Visual Studio 9 2008" -H%CD% -B%CD%\build\msvc32 -DWINDBG_SDK_DIR=%CD%\sdk
+cmake --build %CD%\build\msvc32 --config RelWithDebInfo --target stackdump
+
+cmake -G "Visual Studio 9 2008 Win64" -H%CD% -B%CD%\build\msvc64 -DWINDBG_SDK_DIR=%CD%\sdk
+cmake --build %CD%\build\msvc64 --config RelWithDebInfo --target stackdump
