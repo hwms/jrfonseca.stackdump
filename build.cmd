c0362a9b21a261919fbeef23e3de3f70fd29f23e
José Fonseca <jose.r.fonseca@gmail.com>
2012-06-13 19:37:29 +0100

    Update build script to work with any MSVC version.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,18 @@
-cmake -G "Visual Studio 9 2008" -H%CD% -B%CD%\build\msvc32 -DWINDBG_SDK_DIR=%CD%\sdk
+@echo off
+
+setlocal
+
+
+if not "%VS90COMNTOOLS%"=="" set generator=Visual Studio 9 2008
+if not "%VS100COMNTOOLS%"=="" set generator=Visual Studio 10
+if not "%VS110COMNTOOLS%"=="" set generator=Visual Studio 11
+
+
+cmake -G "%generator%" -H%CD% -B%CD%\build\msvc32 -DWINDBG_SDK_DIR=%CD%\sdk
 cmake --build %CD%\build\msvc32 --config RelWithDebInfo --target stackdump
 
-cmake -G "Visual Studio 9 2008 Win64" -H%CD% -B%CD%\build\msvc64 -DWINDBG_SDK_DIR=%CD%\sdk
+cmake -G "%generator% Win64" -H%CD% -B%CD%\build\msvc64 -DWINDBG_SDK_DIR=%CD%\sdk
 cmake --build %CD%\build\msvc64 --config RelWithDebInfo --target stackdump
+
+
+endlocal
